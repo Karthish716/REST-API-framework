4b3783d13ebb84864927cb7e8206f874034731d3
Karthish716 <karthish2001@gmail.com>
2025-03-20 00:01:46 +0530

    feat: Build REST API framework GET method

---
+++ BuillingSystem/Inventory/views.py
@@ -0,0 +1,13 @@
+from rest_framework .views import APIView
+from rest_framework.response import Response
+from .models import * 
+
+class ProductsView(APIView):
+
+    def post(self, request):
+
+        new_product = Products(products_name = request.data["product_name"], code= request.data['code'], price = request.data['price'])
+
+        new_product.save()
+
+        return Response("Data Saved")
